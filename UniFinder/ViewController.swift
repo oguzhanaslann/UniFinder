f5d24ccda007f57e6c0af793db33cd110eba6242
oguzhanaslann <oguz21199@gmail.com>
2021-09-19 00:17:09 +0300

    Initial Commit

---
+++ UniFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniFinder
+//
+//  Created by Oğuzhan Aslan on 19.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
